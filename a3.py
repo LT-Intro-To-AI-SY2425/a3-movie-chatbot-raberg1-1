968f21dd19511db119bd27d58d6a4afd16413ef0
Robert Berg <rberg@cps.edu>
2024-10-04 10:53:28 -0500

    finished title by year range

--- a3.py
+++ a3.py
@@ -79,7 +79,14 @@ def title_by_year_range(matches: List[str]) -> List[str]:
         a list of movie titles made during those years, inclusive (meaning if you pass
         in ["1991", "1994"] you will get movies made in 1991, 1992, 1993 & 1994)
     """
-    pass
+    start_year = int(matches[0])
+    end_year = int(matches[1])
+    result = []
+    for movie in movie_db:
+        if start_year <= get_year(movie) <= end_year:
+            result.append(get_title(movie))
+    return result
+
 
 
 def title_before_year(matches: List[str]) -> List[str]:
